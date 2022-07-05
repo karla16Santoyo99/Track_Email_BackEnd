855c44a0599ce1db25c8391a31ec2a1bceb4384e
khaliq.Dad <64182574+karla16Santoyo99@users.noreply.github.com>
2022-07-05 17:30:29 -0500

    comit cambios locales

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+//MANERA DE HACER ENVIO DE CORREOS DE FORMA LOCAL 
+//OJO 7_7 SE ENVIO CON CON CORREO PERSONAL Y CREDENCIALES PERSONALES 
+
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 
@@ -13,20 +17,16 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->SMTPDebug = 2;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.digitaleriamx.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'mailing@digitaleriamx.com';                     //SMTP username
-    $mail->Password   = 'NJCf@jEK3&8';                               //SMTP password
+    $mail->Username   = 'karlasantoyo1601@gmail.com';                     //SMTP username
+    $mail->Password   = 'muvdunhbwavstgvt';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
+    $mail->Port       = '587';                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->setFrom('mailing@digitaleriamx.com', 'Mailer');
     $mail->addAddress('judith99santoyo16@gmail.com', 'Joe User');     //Add a recipient
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Here is the subject';
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
