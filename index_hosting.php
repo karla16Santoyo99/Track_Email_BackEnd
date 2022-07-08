98669a2d2d9b8c2ea7a1dff92effb44fd342c920
khaliq.Dad <64182574+karla16Santoyo99@users.noreply.github.com>
2022-07-07 19:20:27 -0500

    Cambio de email solo local

--- index_hosting.php
+++ index_hosting.php
@@ -10,7 +10,7 @@ require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+//require 'vendor/autoload.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
