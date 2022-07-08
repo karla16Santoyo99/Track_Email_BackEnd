0a2974d365de48676d4180c43d51c9e28ded658c
khaliq.Dad <64182574+karla16Santoyo99@users.noreply.github.com>
2022-07-07 19:21:09 -0500

    cambios de smtp local

--- index.php
+++ index.php
@@ -22,9 +22,9 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'karlasantoyo1601@gmail.com';                     //SMTP username
-    $mail->Password   = 'muvdunhbwavstgvt';                               //SMTP password
+    $mail->Password   = 'zzlsksczgdbzmcus';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = '587';                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->setFrom('mailing@digitaleriamx.com', 'Mailer');
     $mail->addAddress('judith99santoyo16@gmail.com', 'Joe User');     //Add a recipient
     $mail->isHTML(true);                                  //Set email format to HTML
